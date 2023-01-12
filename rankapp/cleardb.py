c6e007f18309b6cf8363372a3a772a0d30313a0a
Satan <karelseaat@gmail.com>
2023-01-12 21:46:40 +0100

    doing all

---
+++ rankapp/cleardb.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.append('..')
+
+import random
+import os
+import sqlalchemy
+from sqlalchemy import create_engine
+from faker import Faker
+from sqlalchemy.orm import sessionmaker
+from models import *
+import random
+
+from config import make_session, CONNECTIONURI
+
+def clear_db():
+
+    if CONNECTIONURI and 'sqlite:///' in CONNECTIONURI:
+        realpath = "/"+"/".join(CONNECTIONURI.split('/')[5:])
+        if os.path.exists(realpath):
+            os.remove(realpath)
+        else:
+            print("path does not exist !")
+
+    session = make_session()
+
+    session.commit()
+    session.close()
+
+if __name__ == "__main__":
+    clear_db()
